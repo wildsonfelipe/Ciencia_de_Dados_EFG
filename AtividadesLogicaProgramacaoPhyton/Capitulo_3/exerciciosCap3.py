dbf5db461751c0014dbaa00b9580919f4705acb7
wildsonfelipe <wildson.felipe@gmail.com>
2026-06-09 23:36:51 -0300

    Criado pasta ExerciciosCap3
    Inclusão dos Exercícios do Capítulo3

---
+++ AtividadesLogicaProgramacaoPhyton/Capitulo_3/exerciciosCap3.py
@@ -0,0 +1,68 @@
+# nota = 6
+# media = 7
+# aprovado = nota > media
+# print(aprovado)
+
+# salario_recebido = 2000
+# paga_imposto = salario_recebido > 1200
+# print(paga_imposto)
+
+# materia1 = 8
+# materia2 = 8
+# materia3 = 8
+# aprovado = materia1 >= 7 and materia2 >= 7 and materia3 >= 7
+# print(aprovado)
+
+# A = 1
+# B = 2
+# C = True
+# D = False
+# print (A > B  and C or D)
+
+# A = 10
+# B = 3
+# C = False
+# D = False
+# print (A > B  and C or D)
+
+# A = 5
+# B = 1
+# C = True
+# D = True
+# print (A > B  and C or D)
+
+#3 * 0.1
+#print(3 * 0.1)
+
+#Exercício 3.4 Escreva uma expressão para determinar se uma pessoa deve ou não
+#pagar imposto. Considere que pagam imposto pessoas cujo salário é maior que R$ 1.200,00.
+# salario2 = 1300
+# paga_imposto = salario2 > 1200
+# print(paga_imposto)
+
+#Exercício 3.6 Escreva uma expressão que será utilizada para decidir se um aluno foi
+#ou não aprovado. Para ser aprovado, todas as médias do aluno devem ser maiores
+#que 7. Considere que o aluno cursa apenas três matérias, e que a nota de cada uma
+#está armazenada nas seguintes variáveis: Matérial, Matéria2 e Matéria3. 
+# materia1 = float(input("Digite sua nota"))
+# materia2 = float(input("Digite sua nota"))
+# materia3 = float(input("Digite sua nota"))
+# aprovado = materia1 > 7 and materia2 > 7 and materia3 > 7
+# print("Aprovado:", aprovado)
+
+#Exercício 3.7 Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela
+# n1 = int(input("Digite um número"))
+# n2 = int(input('Digite outro número'))
+# print("a soma de N1 + N2 é", n1 + n2)
+
+#Exercício 3.8 Escreva um programa que leia um valor em metros e o exiba convertido em milímetros. 
+# tamanho = float(input('Digite o tamanho em metros'))
+# print("O tamanho é de", tamanho*1000, "milímetros")
+
+#Exercício 3.9 Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
+# dia = int(input(' Digite a quantidade de dias'))
+# horas = int(input(' Digite a quantidade de horas'))
+# minutos = int(input(' Digite a quantidade de minutos'))
+# segundos = int(input(' Digite a quantidade de segundos'))
+# total = (dia * 86400) + (horas * 3600) + (minutos * 60) + segundos
+# print('O total de segundos é:', total)
