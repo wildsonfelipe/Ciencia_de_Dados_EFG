8915e4b89d9e2e7d301a0c4e5bca2403930d1d39
wildsonfelipe <wildson.felipe@gmail.com>
2026-06-09 21:53:49 -0300

    Primeiras atividades com Python
    Criei as primeiras atividades com Python

---
+++ AtividadesLogicaProgramacaoPhyton/Capitulo_1/exercicios.py
@@ -0,0 +1,37 @@
+#print('Olá Mundo!')
+# Exercício 2.1 Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+# 10 + 20 x 30
+# 41 / 30
+# (94 + 2) X 6 - 1
+
+#print(10 + 20 * 30)
+#print(41 / 30)
+#print((94 + 2) * 6 - 1)
+
+# Exercício 2.2 Digite a seguinte expressão no interpretador: 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+#print(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
+
+# Programa 2.1 - Primeiro programa com variáveis
+# a = 2 
+# b = 3
+# print(a + b)
+
+# nome = 'Wildson Felipe'
+# idade = '44 Anos'
+# print (f'Meu nome é {nome}, tenho {idade}, e faço curso de Ciência de Dados Escola do Futuro do Estado de Goiás')
+
+# a = 3
+# b = 5
+# equacao = (2*a) * (3*b)
+# print ("o Resultado da equação é:", equacao)
+
+# Daniel = 9
+# Alyce = 42
+# Wildson = 44
+# resultado = Daniel + Alyce + Wildson
+# print(f"A Somatória da idade de Daniel, Alyce e Wildson é", resultado)
+
+# salario = 1612
+# aumento = 20
+# salario_final = salario + (salario * aumento/100)
+# print (f"Você recebeu um salario de {salario} e teve um ajuste de {aumento}% o salário final é {salario_final}")
